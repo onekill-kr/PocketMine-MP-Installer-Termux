546c31118afa8408ad58952ced6e671634dc93e5
onekill-kr <52688566+onekill-kr@users.noreply.github.com>
2019-08-18 11:50:14 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,26 +7,26 @@ cd PMMPSERVER
 apt-get update
 apt-get upgrade
 apt-get install php
-echo -e "Y\n" | apt-get php
+echo -e "Y\n" | apt-get install php
 apt-get install make
-echo -e "Y\n" | apt-get make
+echo -e "Y\n" | apt-get install make
 apt-get install autoconf
-echo -e "Y\n" | apt-get autoconf
+echo -e "Y\n" | apt-get install autoconf
 apt-get install m4
-echo -e "Y\n" | apt-get m4
+echo -e "Y\n" | apt-get install m4
 apt-get install getconf
-echo -e "Y\n" | apt-get getconf
+echo -e "Y\n" | apt-get install getconf
 apt-get install bison
-echo -e "Y\n" | apt-get bison
+echo -e "Y\n" | apt-get install bison
 apt-get install g++
-echo -e "Y\n" | apt-get g++
+echo -e "Y\n" | apt-get install g++
 apt-get install git
-echo -e "Y\n" | apt-get git
+echo -e "Y\n" | apt-get install git
 apt-get install cmake
-echo -e "Y\n" | apt-get cmake
+echo -e "Y\n" | apt-get install cmake
 apt-get install libtool
-echo -e "Y\n" | apt-get libtool
+echo -e "Y\n" | apt-get install libtool
 apt-get install libtool-bin
-echo -e "Y\n" | apt-get libtool-bin
+echo -e "Y\n" | apt-get install libtool-bin
 
 wget -q -O -https://get.pmmp.io | bash -s --r
