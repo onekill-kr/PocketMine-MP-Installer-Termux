0abffb8ce29cb00bca0f286303ba938327fba200
onekill-kr <52688566+onekill-kr@users.noreply.github.com>
2020-02-03 14:53:59 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ cd PMMP
 wget https://github.com/onekill-kr/PocketMine-MP-Installer-Termux-/raw/master/php
 wget https://github.com/onekill-kr/PocketMine-MP-Installer-Termux-/raw/master/PocketMine.phar
 wget https://raw.githubusercontent.com/onekill-kr/PocketMine-MP-Installer-Termux-/master/start.sh
-wget https://raw.githubusercontent.com/onekill-kr/PocketMine-MP-Installer-Termux-/master/road.sh
+wget https://raw.githubusercontent.com/onekill-kr/PocketMine-MP-Installer-Termux-/master/load.sh
 
 chmod 777 ./php
 chmod 777 ./start.sh
