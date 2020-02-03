27705a47f0e9ecebabd907430b3508e9bb9a2345
onekill-kr <52688566+onekill-kr@users.noreply.github.com>
2020-02-03 22:37:12 +0900

    Update load.sh

--- load.sh
+++ load.sh
@@ -8,7 +8,7 @@ if [ "${info}" == "p" ];
   rm -r /sdcard/TermuxPMMP/plugin_data/*
   cp -r ./plugin_data/* /sdcard/TermuxPMMP/plugin_data/
   echo 플러그인 데이터를 모두 불러왔습니다.
-else
+if [ "${info}" == "" ];
   rm /sdcard/TermuxPMMP/server.properties
   cp ./ /sdcard/TermuxPMMP/server.properies
   echo 월드를 불러오시려면 ./load.sh w 를 플러그인 데이터를 불러오시려면 ./load.sh p 를 입력해 주십시오.
