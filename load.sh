69328b78cdb1e8c3b14181bbaebc0bfab23d88ef
onekill-kr <52688566+onekill-kr@users.noreply.github.com>
2020-02-04 10:33:52 +0900

    Update load.sh

--- load.sh
+++ load.sh
@@ -14,7 +14,7 @@ else
   rm -r /sdcard/TermuxPMMP/plugin_data/
   cp -r ./plugin_data/* /sdcard/TermuxPMMP/plugin_data/
   rm /sdcard/TermuxPMMP/server.properties
-  cp ./server.properties /sdcard/TermuxPMMP/server.properies
+  cp ./server.properties /sdcard/TermuxPMMP/server.properties
   echo 모두 불러왔습니다.
   echo 월드만 불러오시려면 ./load.sh w 를 플러그인 데이터만 불러오시려면 ./load.sh p 를 입력해 주십시오.
 fi
